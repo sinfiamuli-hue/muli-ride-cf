8274efce86cde1f656ea4b36926c6406c28494e5
sinfiamuli-hue <sinfiamuli@gmail.com>
2026-09-24 22:04:26 +0500

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const C='muli-ride-v1',SHELL=['/','/manifest.webmanifest','/icon-192.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(SHELL)));self.skipWaiting()});
+const C='muli-ride-v2',SHELL=['/','/manifest.webmanifest','/icon-192.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>Promise.allSettled(SHELL.map(u=>c.add(u)))));self.skipWaiting()});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!=C).map(x=>caches.delete(x))))));
 self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(e.request.method!='GET'||u.pathname.startsWith('/api/'))return;
  e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(C).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('/'))))});
